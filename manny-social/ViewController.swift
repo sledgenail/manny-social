1018524f582fc76b4155b4040df783bd19f27ef2
Emmanuel Erilibe <sledgenail@gmail.com>
2017-01-11 22:02:15 -0600

    Initial Commit

---
+++ manny-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  manny-social
+//
+//  Created by Emmanuel Erilibe on 1/11/17.
+//  Copyright © 2017 Emmanuel Erilibe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
