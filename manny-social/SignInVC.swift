e80d05493ffda7857af528428e67c6cf20e7b4f2
Emmanuel Erilibe <sledgenail@gmail.com>
2017-01-14 06:22:57 -0600

    finished authentication with email

--- manny-social/SignInVC.swift
+++ manny-social/SignInVC.swift
@@ -12,6 +12,8 @@ import FBSDKLoginKit
 import Firebase
 
 class SignInVC: UIViewController {
+    @IBOutlet weak var emailTextField: FancyField!
+    @IBOutlet weak var passwordTextField: FancyField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +52,24 @@ class SignInVC: UIViewController {
                 print("MANNY: Successfully authenticated with Firebase")
             }
         })
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailTextField.text, let pwd = passwordTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("MANNY: Email user authenticated with Firebase \(error)")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("MANNY: Unable to authenticate with firebase using email \(error)")
+                        } else {
+                            print("MANNY: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
     }
 }
 
