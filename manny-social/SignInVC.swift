6792812822d7ffb3895ede1dbcc67619fad5990e
Emmanuel Erilibe <sledgenail@gmail.com>
2017-01-13 22:13:56 -0600

    Successfully integratedd Firebase and Facebook

--- manny-social/SignInVC.swift
+++ manny-social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,33 @@ class SignInVC: UIViewController {
 
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                print("MANNY: Unable ro authenticate with Facebook: \(error)")
+            } else if result?.isCancelled == true {
+                print("MANNY: User cancelled Facebook authentication")
+            } else {
+                print("MANNY: Succcessfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+        }
+    }
 
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("MANNY: Unable to authenticate with Firebase: \(error)")
+            } else {
+                print("MANNY: Successfully authenticated with Firebase")
+            }
+        })
+    }
 }
 
