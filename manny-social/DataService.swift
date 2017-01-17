a989e1f336c64ebf586c09be27af4af94ae92c7b
Emmanuel Erilibe <sledgenail@gmail.com>
2017-01-16 22:11:40 -0600

    Completely created the login process with an authentication database

---
+++ manny-social/DataService.swift
@@ -0,0 +1,38 @@
+//
+//  DataService.swift
+//  manny-social
+//
+//  Created by Emmanuel Erilibe on 1/16/17.
+//  Copyright © 2017 Emmanuel Erilibe. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+let DB_BASE = FIRDatabase.database().reference()
+
+class DataService {
+    
+    static let ds = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("posts")
+    private var _REF_USER = DB_BASE.child("users")
+    
+    var REF_BASE: FIRDatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_POSTS: FIRDatabaseReference {
+        return _REF_POSTS
+    }
+    
+    var REF_USERS: FIRDatabaseReference {
+        return _REF_USER
+    }
+    
+    func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+}
--- manny-social/SignInVC.swift
+++ manny-social/SignInVC.swift
@@ -55,7 +55,8 @@ class SignInVC: UIViewController {
             } else {
                 print("MANNY: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -67,7 +68,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("MANNY: Email user authenticated with Firebase \(error)")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -76,7 +78,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("MANNY: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -85,7 +88,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("MANNY: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
