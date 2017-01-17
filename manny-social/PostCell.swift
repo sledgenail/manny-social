c222736ec828e420ed79837e8743348aa2d7de6a
Emmanuel Erilibe <sledgenail@gmail.com>
2017-01-17 16:28:01 -0600

    Successfully downloaded from firebase

--- manny-social/PostCell.swift
+++ manny-social/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
 
@@ -23,9 +24,40 @@ class PostCell: UITableViewCell {
     }
     
 
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLabel.text = "\(post.likes)"
+        
+        if img != nil {
+            
+            self.postImg.image = img
+            
+        } else {
+            
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                
+                if error != nil {
+                    
+                    print("MANNY: Unable to download image from Firebase storage")
+                } else {
+                    
+                    print("MANNY: image downloaded from Firebase storage")
+                    
+                    if let imgData = data {
+                        
+                        if let img = UIImage(data: imgData){
+                            
+                            self.postImg.image = img
+                            
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                            
+                        }
+                    }
+                }
+            })
+        }
     }
 }
